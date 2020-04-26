1a9fd3ced6ec438e2342a66498daec98d43a2936
akash <akashroy7796@gmail.com>
2020-04-25 21:46:49 -0400

    update working in a global string

--- SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtHelper.java
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtHelper.java
@@ -1,9 +1,12 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 public class SimpleDhtHelper {
     final static int[] remote_ports= new int[]{11108, 11112, 11116, 11120,11124};
+
+    HashMap<String,Integer> emulatorHash= new HashMap<String, Integer>();
     protected HashMap<Integer,Integer> fillHashMap(HashMap<Integer,Integer> input) {
         int emulator_number=0;
         for(int i:remote_ports)
@@ -12,4 +15,12 @@ public class SimpleDhtHelper {
         }
         return input;
     }
+
+//    public int genratePort(String h) throws NoSuchAlgorithmException {
+//        for ( int i:remote_ports){
+//            int j=i/2;
+//            emulatorHash.put(provider.genHash(Integer.toString(j)),i);
+//        }
+//        return emulatorHash.get(h);
+//    }
 }
--- SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -16,6 +16,8 @@ import java.util.Comparator;
 import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
@@ -39,12 +41,16 @@ public class SimpleDhtProvider extends ContentProvider {
     static final String local_identifier="@";
     static final int AVD0_PORT=11108;
     int clientPort=0;
+    final static int[] remote_ports= new int[]{11108, 11112, 11116, 11120,11124};
     SimpleDhtHelper helper=new SimpleDhtHelper();
-    static  final String[] operations= new String[]{"JOIN","INSERT","QUERY","DELETE"};
+    static  final String[] operations= new String[]{"JOIN","SUCC_SEQUENCE","PRED_SEQUENCE","INSERT","QUERY","DELETE"};
     List<String> nodes = new ArrayList<String>();
-    List<ChordNode> chordNodes=new ArrayList<ChordNode>();
     HashMap<Integer,Integer> emulatorMap=new HashMap<Integer, Integer>();
-
+    ConcurrentHashMap<String,Integer> emulatorHash=new ConcurrentHashMap<String,Integer>();
+    HashMap<String,String> successorMap= new HashMap<String, String>();
+    HashMap<String,String> predecessorMap= new HashMap<String, String>();
+    String sequenceSucc=null;
+    String sequencePred=null;
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -117,23 +123,33 @@ public class SimpleDhtProvider extends ContentProvider {
         Log.d("AVD",portStr);
         final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
         clientPort=Integer.parseInt(myPort);
+//        try {
+//            emulatorHash.put(genHash(myPort),Integer.parseInt(myPort));
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
         Log.d("onCreate",Integer.toString(clientPort));
 
         //If the port is not avd0 send a join request
-
-        if(clientPort!=AVD0_PORT){
-            int emulator=emulatorMap.get(AVD0_PORT);
-            String hashed_emulator= null;
-            try {
-                hashed_emulator = genHash(Integer.toString(emulator));
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-            Log.d("Emu",hashed_emulator+" "+emulator);
-            nodes.add(hashed_emulator);
-            String port=Integer.toString(clientPort);
-            new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,port+":"+"JOIN");
-        }
+//
+//        if(clientPort!=AVD0_PORT){
+//            int emulator=emulatorMap.get(AVD0_PORT);
+//            String hashed_emulator= null;
+//            try {
+//                hashed_emulator = genHash(Integer.toString(emulator));
+//            } catch (NoSuchAlgorithmException e) {
+//                e.printStackTrace();
+//            }
+//
+//            if(!nodes.contains(hashed_emulator)){
+//            nodes.add(hashed_emulator);
+//            emulatorHash.put(hashed_emulator,emulator);
+//            Log.d("Emu",hashed_emulator+" "+emulator);
+//            }
+//            String port=Integer.toString(clientPort);
+//            new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,port+":"+"JOIN");
+//        }
+        new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,myPort+":"+"JOIN");
         return false;
     }
 
@@ -150,7 +166,7 @@ public class SimpleDhtProvider extends ContentProvider {
         return 0;
     }
 
-    private String genHash(String input) throws NoSuchAlgorithmException {
+    public String genHash(String input) throws NoSuchAlgorithmException {
         MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
         byte[] sha1Hash = sha1.digest(input.getBytes());
         Formatter formatter = new Formatter();
@@ -167,6 +183,7 @@ public class SimpleDhtProvider extends ContentProvider {
     private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
         int sequence_number=0;
         int i=0;
+        int j=0;
         Uri providerUri= Uri.parse("content://edu.buffalo.cse.cse486586.groupmessenger2.provider");
         @Override
         protected Void doInBackground(ServerSocket... sockets) {
@@ -174,6 +191,7 @@ public class SimpleDhtProvider extends ContentProvider {
             try {
                 while (true) {
                     Log.d("Server","Message in Server");
+                    Log.d("Server","Trying to connect");
                     Socket socket = serverSocket.accept();
                     Log.d("Server", "Connection Successful");
                     DataInputStream inputStream = new DataInputStream(socket.getInputStream());
@@ -182,38 +200,122 @@ public class SimpleDhtProvider extends ContentProvider {
                     String[] messages=receivedMessage.split(":");
                     int receivedPort=Integer.parseInt(messages[0]);
                     String operation=messages[1];
-                    String successor=messages[2];
-                    String predecessor=messages[3];
                     Log.d("Server",messages[0]);
-                    Log.d("Operation",messages[1]);
+                    Log.d("Operation Here",messages[1]);
+                    Log.d("Server1","Here");
                     if(operations[0].equals(operation))
                     {
                         int emulator=emulatorMap.get(receivedPort);
+                        ChordNode chordNode=new ChordNode();
                         String hashed_emulator=genHash(Integer.toString(emulator));
                         Log.d("Emu",hashed_emulator+" "+emulator);
                         nodes.add(hashed_emulator);
+                        emulatorHash.put(hashed_emulator,emulator);
                         i++;
                         Collections.sort(nodes,new CustomComparator());
-                        if(i==4)
+                        if(i==5)
                         {
-                            Log.d("zzzzz",nodes.get(0));
-                            Log.d("zzzzz",nodes.get(1));
-                            Log.d("zzzzz",nodes.get(2));
-                            Log.d("zzzzz",nodes.get(3));
-                            Log.d("zzzzz",nodes.get(4));
+                            Log.d("Sorted Nodes",nodes.get(0));
+                            Log.d("Sorted Nodes",nodes.get(1));
+                            Log.d("Sorted Nodes",nodes.get(2));
+                            Log.d("Sorted Nodes",nodes.get(3));
+                            Log.d("Sorted Nodes",nodes.get(4));
                         }
+                        int nodePosition=nodes.indexOf(hashed_emulator);
+                        Log.d("Np",Integer.toString(nodePosition));
+                        //Set Successor
+                        //Last Node of the Chord
+                        if(nodePosition==nodes.size()-1){
+                            chordNode.setSuccessor(nodes.get(0));
+                        }
+                        else {
+                            Log.d("Succ",nodes.get(nodePosition+1));
+                            chordNode.setSuccessor(nodes.get(nodePosition+1));
+                        }
+                        String clientSuccessorOutputString=receivedPort+":"+"SUCC_SEQUENCE"+":"+ chordNode.getSuccessor()+":"+receivedPort;
+                        Log.d("Server","Sending Successor to Client"+clientSuccessorOutputString);
+                        callClient(clientSuccessorOutputString);
+
+                        //Set Predecessor
+                        //First Node of the Chord
+                        if(nodePosition==0){
+                            Log.d("pred",Integer.toString(nodes.size()));
+                            Log.d("pred",nodes.get(nodes.size()-1));
+                            chordNode.setPredecessor(nodes.get(nodes.size()-1));
+                        }
+                        else {
+                            chordNode.setPredecessor(nodes.get(nodePosition-1));
+                        }
+                        String clientPredOutputString=receivedPort+":"+"PRED_SEQUENCE"+":"+ chordNode.getPredecessor()+":"+receivedPort;
+                        Log.d("Server","Sending Predecessor to Client"+clientPredOutputString);
+                        callClient(clientPredOutputString);
+                    }
+                    else if (operations[1].equals(operation)){
+                        Log.d("Server","In Successor Sequence");
+                        int emulator=emulatorMap.get(receivedPort);
+                        String hashed_emu=genHash(Integer.toString(emulator));
+                        String successor=messages[2];
+                        Log.d("Successor",messages[2]);
+                      //  successorMap.put(hashed_emu,successor);
+                        sequenceSucc=successor;
+                        Log.d("Successor Added",hashed_emu+": S"+" "+successor);
+                        Log.d("Here","Before getting");
+                        int succEmulator=Integer.parseInt(messages[3]);
+                        Log.d("Succ Port",Integer.toString(succEmulator));
+                        int port_emu=succEmulator;
+                        int e=succEmulator/2;
+                        Log.d("Succ Port",Integer.toString(port_emu));
+                        String port=Integer.toString(port_emu);
+                        String clientMessage=port+":"+"PRED_SEQUENCE"+":"+hashed_emu+":"+receivedPort;
+                        Log.d("Sending Pred to client",clientMessage);
+                        Log.d("Received Port",Integer.toString(receivedPort));
+                        Log.d("Emu",Integer.toString(port_emu));
+                        Log.d("PreMap",genHash(Integer.toString(e)));
+                        if(!Integer.toString(receivedPort).equals(port)) {
+                            if (!port.equals(messages[4])){
+                                callClient(clientMessage);
+                            }
+
+                        }
+                        Log.d("Successor",sequenceSucc);
+                    }
+                    else if(operations[2].equals(operation)){
+                        Log.d("Server","IN Predecessor Sequence");
+                        int emulator=emulatorMap.get(receivedPort);
+
+                        String hashed_emu=genHash(Integer.toString(emulator));
+                        String predecessor=messages[2];
+                        Log.d("Predecessor",messages[2]);
+                      //  predecessorMap.put(hashed_emu,predecessor);
+                        sequencePred=predecessor;
+                        Log.d("Predecessor Added",hashed_emu+": P"+" "+predecessor);
+                        int predEmulator=Integer.parseInt(messages[3]);
+                        int port_emu=predEmulator;
+                        String port=Integer.toString(port_emu);
+                        String clientMessage=port+":"+"SUCC_SEQUENCE"+":"+hashed_emu+":"+receivedPort;
+                        Log.d("Sending Succ to client",clientMessage);
+                        Log.d("Port",port);
+                        Log.d("Received Port",Integer.toString(receivedPort));
+                        if(!Integer.toString(receivedPort).equals(port)) {
+                            if (!port.equals(messages[4])){
+                                callClient(clientMessage);
+                            }
+                        }
+                        Log.d("Predecessor",sequencePred);
                     }
                 }
             }
             catch (Exception ex)
             {
-                ex.getMessage();
+                ex.printStackTrace();
             }
 
             return null;
         }
 
-
+        protected void callClient(String message){
+            new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,message);
+        }
         protected void onProgressUpdate(String...strings) {
             /*
              * The following code displays what is received in doInBackground().
@@ -240,6 +342,7 @@ public class SimpleDhtProvider extends ContentProvider {
         @Override
         protected Void doInBackground(String... msgs) {
             String requested_message=msgs[0];
+            SimpleDhtHelper helper=new SimpleDhtHelper();
             String[] messages=requested_message.split((":"));
             String operation= messages[1];
             if(operation.equals(operations[0])){
@@ -253,19 +356,63 @@ public class SimpleDhtProvider extends ContentProvider {
                     String client_message=requested_message+":"+successor+":"+predecessor;
                     Log.d("Message to send",client_message);
                     outputStream.writeUTF(client_message);
-                    outputStream.flush();
-
                 } catch (Exception e) {
-                    Log.e(TAG, "ClientTask UnknownHostException");
+                    Log.e(TAG, "ClientTask UnknownHostException S");
                 }
             }
-            else{
+            else if(operation.equals(operations[1])){
                 try {
+                    String receivedPortFromServer=messages[0];
+                    String successor=messages[2];
+                    Log.d("Client",successor);
+                    int e=0;
+                    for (int i:remote_ports){
+                        Log.d("Hash",genHash(Integer.toString(i/2)));
+                        Log.d("P",successor);
+                        if(genHash(Integer.toString(i/2)).equals(successor))
+                        {
+                            e=i;
+                        }
+                        Log.d("e",Integer.toString(e));
+                    }
 
+                    Log.d("Port Succ Update",receivedPortFromServer);
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt(receivedPortFromServer));
+                    DataOutputStream outputStream=new DataOutputStream(socket.getOutputStream());
+                    String outputToServer=receivedPortFromServer+":"+"SUCC_SEQUENCE"+":"+successor+":"+e+":"+messages[3];
+                    Log.d("Client Sending String",outputToServer);
+                    outputStream.writeUTF(outputToServer);
                 }
                 catch (Exception ex)
                 {
-
+                    ex.printStackTrace();
+                }
+            }
+            else if(operation.equals(operations[2])){
+                try {
+                    String receivedPortFromServer=messages[0];
+                    String predecessor=messages[2];
+                    Log.d("Client",predecessor);
+                    int e=0;
+                    for (int i:remote_ports){
+                        Log.d("Hash",genHash(Integer.toString(i/2)));
+                        Log.d("P",predecessor);
+                        if(genHash(Integer.toString(i/2)).equals(predecessor))
+                        {
+                            e=i;
+                        }
+                        Log.d("e",Integer.toString(e));
+                    }
+                    Log.d("Port Pred Update",receivedPortFromServer);
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt(receivedPortFromServer));
+                    DataOutputStream outputStream=new DataOutputStream(socket.getOutputStream());
+                    String outputToServer=receivedPortFromServer+":"+"PRED_SEQUENCE"+":"+predecessor+":"+e+":"+messages[3];
+                    Log.d("Client Sending String",outputToServer);
+                    outputStream.writeUTF(outputToServer);
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
                 }
             }
 
